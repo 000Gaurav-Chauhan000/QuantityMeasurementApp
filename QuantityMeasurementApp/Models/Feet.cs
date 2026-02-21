55eb0a9db373a6165c3f2c82e148e9349cf63da1
shubhamchauhan35725@gmail.com <shubhamchauhan35725@gmail.com>
2026-02-21 21:09:07 +0530

    [Gaurav-Chauhan].Add Feet Equality Functionality

---
+++ QuantityMeasurementApp/Models/Feet.cs
@@ -0,0 +1,27 @@
+namespace QuantityMeasurementApp.Models
+{
+    internal class Feet : IEquatable<Feet>
+    {
+        public double Data { get; }
+
+        public Feet(double data)
+        {
+            if(data < 0)
+            {
+                throw new ArgumentException("Feet values can't be Negative !!");
+            }
+            Data = data;
+        }
+       
+
+        public bool Equals(Feet? other)
+        {
+            if (other is null) return false;
+            return Data.CompareTo(other.Data) == 0;
+        }
+
+        public override bool Equals(object? obj) => Equals(obj as Feet);
+
+        public override int GetHashCode() => Data.GetHashCode();
+    }
+}
