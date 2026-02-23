07411b56c8ef844819ea67afb8b158577d70be5c
shubhamchauhan35725@gmail.com <shubhamchauhan35725@gmail.com>
2026-02-23 21:11:03 +0530

    [Gaurav-Chauhan].Add feature/QuantityLengthAddition

--- QuantityMeasurementApp/Models/QuantityLength.cs
+++ QuantityMeasurementApp/Models/QuantityLength.cs
@@ -4,59 +4,105 @@ namespace QuantityMeasurementApp.Models
 {
     public class QuantityLength
     {
-        private const double Tolerance = 1e-9;
+         private readonly double _value;
+        private readonly LengthUnit _unit;
 
-        public double Value { get; }
-        public LengthUnit Unit { get; }
+        private const double INCH_TO_CM = 2.54;
+        private const double EPSILON = 1e-6;
 
         public QuantityLength(double value, LengthUnit unit)
         {
             if (double.IsNaN(value) || double.IsInfinity(value))
-                throw new ArgumentException("Value must be a finite number.", nameof(value));
+                throw new ArgumentException("Invalid numeric value");
 
-            Value = value;
-            Unit = unit;
+            _value = value;
+            _unit = unit;
         }
 
-        public double ConvertToFeet()
+        public double Value => _value;
+        public LengthUnit Unit => _unit;
+
+
+        public QuantityLength Add(QuantityLength other)
         {
-            return Value * Unit.ToFeetFactor();
-        }
+            if (other is null)
+                throw new ArgumentException("Second operand cannot be null");
 
-        public double ConvertTo(LengthUnit targetUnit)
+            double thisInches = ConvertToInches(_value, _unit);
+            double otherInches = ConvertToInches(other._value, other._unit);
+
+            double sumInches = thisInches + otherInches;
+
+            double resultValue = ConvertFromInches(sumInches, _unit);
+
+            return new QuantityLength(resultValue, _unit);
+        }
+        public QuantityLength ConvertTo(LengthUnit targetUnit)
+{
+    double converted = Convert(_value, _unit, targetUnit);
+    return new QuantityLength(converted, targetUnit);
+}
+
+        public static QuantityLength Add(QuantityLength first, QuantityLength second)
         {
-            double inFeet = ConvertToFeet();
-            return inFeet / targetUnit.ToFeetFactor();
+            if (first is null || second is null)
+                throw new ArgumentException("Operands cannot be null");
+
+            return first.Add(second);
         }
 
-        public static double Convert(double value, LengthUnit sourceUnit, LengthUnit targetUnit)
+
+        public static double Convert(double value, LengthUnit source, LengthUnit target)
         {
-            if (double.IsNaN(value) || double.IsInfinity(value))
-                throw new ArgumentException("Value must be a finite number.", nameof(value));
-
-            double inFeet = value * sourceUnit.ToFeetFactor();
-            return inFeet / targetUnit.ToFeetFactor();
+            double inches = ConvertToInches(value, source);
+            return ConvertFromInches(inches, target);
         }
 
-        
+        private static double ConvertToInches(double value, LengthUnit unit)
+        {
+            return unit switch
+            {
+                LengthUnit.INCHES => value,
+                LengthUnit.FEET => value * 12,
+                LengthUnit.YARDS => value * 36,
+                LengthUnit.CENTIMETERS => value / INCH_TO_CM,
+                _ => throw new ArgumentException("Unsupported unit")
+            };
+        }
+
+        private static double ConvertFromInches(double inches, LengthUnit target)
+        {
+            return target switch
+            {
+                LengthUnit.INCHES => inches,
+                LengthUnit.FEET => inches / 12,
+                LengthUnit.YARDS => inches / 36,
+                LengthUnit.CENTIMETERS => inches * INCH_TO_CM,
+                _ => throw new ArgumentException("Unsupported unit")
+            };
+        }
+
+        // ------------------ EQUALITY ------------------
+
         public override bool Equals(object? obj)
         {
-            if (obj is null) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj is not QuantityLength other) return false;
+            if (obj is not QuantityLength other)
+                return false;
 
-            return Math.Abs(ConvertToFeet() - other.ConvertToFeet()) <= Tolerance;
+            double thisInches = ConvertToInches(_value, _unit);
+            double otherInches = ConvertToInches(other._value, other._unit);
+
+            return Math.Abs(thisInches - otherInches) < EPSILON;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(ConvertToFeet());
+            return ConvertToInches(_value, _unit).GetHashCode();
         }
 
-        public static bool operator ==(QuantityLength? left, QuantityLength? right) =>
-            left is null ? right is null : left.Equals(right);
-
-        public static bool operator !=(QuantityLength? left, QuantityLength? right) =>
-            !(left == right);
+        public override string ToString()
+        {
+            return $"Quantity({_value}, {_unit})";
+        }
     }
 }
